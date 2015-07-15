e8396c427ff2e240702930da444383f685981fc2
CFluciano <luciano.demaria@itcloud.it>
2015-07-15 17:43:36 +0200

    new app 2

---
+++ app/src/main/java/com/helloworldluciano/com/firstapplication/SecondActivity.java
@@ -0,0 +1,48 @@
+package com.helloworldluciano.com.firstapplication;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class SecondActivity extends ActionBarActivity {
+    public final static String MESSAGE_KEY="com.helloworldluciano.com.firstapplication.message_key";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        String message_willkommen = "string/willkommen";
+        String message = intent.getStringExtra(MESSAGE_KEY);
+        TextView textView = new TextView(this);
+        textView.setTextSize(45);
+        textView.setText(message_willkommen+message);
+        setContentView(textView);
+        //setContentView(R.layout.second_layout);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_second, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
